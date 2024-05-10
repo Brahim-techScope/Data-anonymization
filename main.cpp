9a20ad5b1a398e6e91e8151b61330cbb84a84158
Brahim-techScope <144385307+Brahim-techScope@users.noreply.github.com>
2024-05-10 15:34:51 +0200

    update main

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ int main() {
         Dataset y_train("data/true_labels.eng.train.csv", true, false);
         
         // Parameters for logistic regression
-        double lr = 0.01;
-        long m_epochs = 100;
+        double lr = 0.001;
+        long m_epochs = 600;
 
         // Create an instance of LogisticRegression and fit the model.
         cout << "\nFitting the model..." << endl;
@@ -64,8 +64,8 @@ int main() {
         Dataset y_train("data/true_labels.eng.train.csv", false, true);
         
         // Parameters for logistic regression for multiclass classification
-        double lr = 0.005;
-        long m_epochs = 1000;
+        double lr = 0.001;
+        long m_epochs = 500;
 
         // Create an instance of MulticlassClassifier and fit the model
         cout << "\nFitting the model..." << endl;
@@ -89,6 +89,17 @@ int main() {
         cout << "Precision on test set a: " << classifier.precision(X_test_a, y_test_a) << endl;
         cout << "Recall on test set a: " << classifier.recall(X_test_a, y_test_a) << endl;
         cout << "F1 score on test set a: " << classifier.f1_score(X_test_a, y_test_a) << endl;
+
+        // metrics on the test set b
+        cout << "\nEvaluating the model on the test set b..." << endl;
+        Dataset X_test_b("data/representation.eng.testb.csv");
+        Dataset y_test_b("data/true_labels.eng.testb.csv", false, true);
+
+        classifier.show_confusion_matrix(X_test_b, y_test_b);
+        cout << "Accuracy on test set b: " << classifier.accuracy(X_test_b, y_test_b) << endl;
+        cout << "Precision on test set b: " << classifier.precision(X_test_b, y_test_b) << endl;
+        cout << "Recall on test set b: " << classifier.recall(X_test_b, y_test_b) << endl;
+        cout << "F1 score on test set b: " << classifier.f1_score(X_test_b, y_test_b) << endl;
     } else {
         cout << "Invalid input !" << endl;
     }
